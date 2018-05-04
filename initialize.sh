ff1f9b866fd42a662836fb340aca3c57d1ba3f1e
afeiship <1290657123@qq.com>
2018-05-04 10:50:46 +0800

    issue: git encoding filename with utf8

--- initialize.sh
+++ initialize.sh
@@ -5,5 +5,10 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 git config --global user.email "1290657123@qq.com";
 git config --global user.name "feizheng";
 
+## chinese encoding problem: http://www.cnblogs.com/Jerryshome/archive/2012/04/19/2457139.html
+git config --global core.quotepath false;
+
 ##ignore output:
 ssh-add ~/.ssh/id_rsa.github.com >/dev/null 2>/dev/null;
+
+
