88c9bfb5d307ccd9149a4f5fe56759699e345ba4
afeiship <1290657123@qq.com>
2019-07-08 16:19:36 +0800

    add: gs->gst

--- index.sh
+++ index.sh
@@ -33,7 +33,7 @@ alias gckd='git checkout develop';
 alias gckdev='git checkout dev';
 
 ## git others:
-alias gs='git status ';
+alias gst='git status ';
 alias ga='git add ';
 alias gb='git branch ';
 alias gd='git diff';
