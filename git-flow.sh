f44c1584e3ca0b95c7dff18e5767671fce6af56d
afeiship <1290657123@qq.com>
2020-03-24 18:46:29 +0800

    add: gff

--- git-flow.sh
+++ git-flow.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+alias gff="git-flow"
 alias gf="git flow"
-alias gf-start="git flow feature start "
-alias gf-finish="git flow feature finish "
+alias gf-init="git flow init "
+
+# features
+alias gff-start="git flow feature start "
+alias gff-finish="git flow feature finish "
+alias gff-publish="git flow feature publish "
+alias gff-track="git flow feature track "
+
+# release
