54f6ee2af631df31e042320fafa3274b935761c9
afeiship <1290657123@qq.com>
2023-03-25 20:50:31 +0800

    feat: better gcd

--- modules/01-gcd.sh
+++ modules/01-gcd.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 GIT_URL=$1;
 dir=`basename $GIT_URL .git`;
+orgname=`echo $GIT_URL | awk -F [:/] '{print$2}'`;
 
-if [ -d $dir ]; then
-    cd $dir && git pull;
+# A special case for `afeiship` org.
+if [ $orgname == 'afeiship' ]; then
+    orgname='github';
+fi
+
+localdir="$HOME/$orgname/$dir";
+
+# test local dir,  if exists, cd to it and pull, else mkdir and clone
+if [ -d $localdir ]; then
+    cd $localdir && git pull;
 else
+    subdir=`dirname $localdir`;
+    mkdir -p $subdir && cd $subdir;
     git clone $GIT_URL && cd $dir;
 fi
