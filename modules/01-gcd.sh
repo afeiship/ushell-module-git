095e7be5e94148b66adcc65ffff3e36af60a4185
afeiship <1290657123@qq.com>
2023-04-03 17:39:52 +0800

    feat: add orgname for syabot

--- modules/01-gcd.sh
+++ modules/01-gcd.sh
@@ -2,10 +2,15 @@
 GIT_URL=$1;
 dir=`basename $GIT_URL .git`;
 orgname=`echo $GIT_URL | awk -F [:/] '{print$2}'`;
+repourl=`echo $GIT_URL | awk -F [:/] '{print$1}'`;
 
 # A special case for nono `aric-` org.
 if [[ $orgname != "aric-"* ]]; then
     orgname="github";
+fi
+
+if [[ $repourl == 'git@git.saybot.net' ]]; then
+    orgname="saybot";
 fi
 
 localdir="$HOME/$orgname/$dir";
