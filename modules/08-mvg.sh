cef16dbfab6399f4e43fee23ee3947b585d994bd
afeiship <1290657123@qq.com>
2024-01-23 21:33:26 +0800

    feat: folder name

--- modules/08-mvg.sh
+++ modules/08-mvg.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# 1.from $1 or ls -d */ | awk '{printf $1}'
-subdir=$1 || subdir=$(ls -d */ | awk '{printf $1}')
+# 1.from folder name: ls -d */ | awk '{printf $1}'
+subdir=$(ls -d */ | awk '{printf $1}')
 # 2. remove .git directory
 cd $subdir && rm -rf .git && cd ..
 # 3. move all files to parent directory
