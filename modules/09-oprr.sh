3910216e885bbec4c61dd0f0f43ddabf2192baba
afeiship <1290657123@qq.com>
2024-09-01 13:56:25 +0800

    fix: repo path

--- modules/09-oprr.sh
+++ modules/09-oprr.sh
@@ -7,10 +7,10 @@ BASE_BRANCH=${1:-dev}
 GIT_URL=$(git config --get remote.origin.url)
 
 # 从 GIT_URL 中 获取 applyai-dev/applyai-frontend 
-PROJECT_NAME=$(echo $GIT_URL | awk -F'/' '{print $4}')
+REPO_PATH=$(echo "$GIT_URL" | awk -F'[:/]' '{print $(NF-1)"/"$NF}' | cut -d '.' -f 1)
 
 # 获取当前分支名称
 CURRENT_BRANCH=$(git branch --show-current)
 
 # 打开 GitHub 对比页面
-open "https://github.com/$PROJECT_NAME/compare/$BASE_BRANCH...$CURRENT_BRANCH?expand=1"
+open "https://github.com/$REPO_PATH/compare/$BASE_BRANCH...$CURRENT_BRANCH?expand=1"
