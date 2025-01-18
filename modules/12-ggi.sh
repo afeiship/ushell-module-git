a069b4c69baba98bbc36b448d63c7f7a03b26971
afeiship <1290657123@qq.com>
2025-01-18 15:45:20 +0800

    feat: add automated commit message pick process using expect

---
+++ modules/12-ggi.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/expect
+
+# 设置超时时间（-1 表示无超时）
+set timeout -1
+
+spawn git add .
+
+# 启动 aic2 命令
+spawn aic2 --all -y
+
+# 等待交互提示
+expect {
+    "Pick a commit message to use:" {
+        # 模拟按下回车键选择默认选项
+        send "\r"
+        exp_continue
+    }
+    eof
+}
+
+# 在完成 aic2 后执行 git push
+spawn git push
+
+# 捕获 git push 的输出并结束脚本
+expect eof
+
