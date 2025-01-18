ea1da48e9f60f37b8a47d931a960bb0c849fe373
afeiship <1290657123@qq.com>
2025-01-18 15:54:21 +0800

    refactor: optimize ggi.sh script for logging

--- modules/12-ggi.sh
+++ modules/12-ggi.sh
@@ -3,13 +3,14 @@
 # 设置超时时间（-1 表示无超时）
 set timeout -1
 
-# 禁用日志输出
-log_user 0
-
 spawn git add .
 
 # 启动 aic2 命令
 spawn aic2 --all -y
+
+# 禁用日志输出
+log_user 0
+
 
 # 等待交互提示
 expect {
@@ -20,6 +21,9 @@ expect {
     }
     eof
 }
+
+# 启用日志输出（仅对 git push 的结果显示）
+log_user 1
 
 # 在完成 aic2 后执行 git push
 spawn git push
