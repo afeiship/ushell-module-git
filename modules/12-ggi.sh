1b0ed0310cc42d126855999883117ba6414e7f3f
afeiship <1290657123@qq.com>
2025-01-18 15:55:57 +0800

    refactor: clean up logging configuration in modules/12-ggi.sh

--- modules/12-ggi.sh
+++ modules/12-ggi.sh
@@ -3,10 +3,10 @@
 # 设置超时时间（-1 表示无超时）
 set timeout -1
 
-spawn git add .
-
 # 禁用日志输出
-log_user 0
+# log_user 0
+
+spawn git add .
 
 # 启动 aic2 命令
 spawn aic2 --all -y
@@ -20,9 +20,6 @@ expect {
     }
     eof
 }
-
-# 启用日志输出（仅对 git push 的结果显示）
-log_user 1
 
 # 在完成 aic2 后执行 git push
 spawn git push
